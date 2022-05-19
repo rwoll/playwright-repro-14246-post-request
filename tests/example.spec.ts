1d33ac39ccf485d2417c8319dea9c587e7e9f6d2
Ross A. Wollman <ross.wollman@gmail.com>
2022-05-19 03:10:54 -0700

    use fixture like user

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,21 +1,31 @@
-import { test, expect, Page } from '@playwright/test';
+import { test as _test, expect, APIRequestContext } from '@playwright/test';
+
+class MyAPI {
+  private request: APIRequestContext;
+
+  constructor(request: APIRequestContext) {
+    this.request = request;
+  }
+
+  async newOnboardingCompany() {
+    const resp = await this.request.post('http://localhost:4747',{ data: { name: 'foo' }});
+    const {id } = await resp.json()
+    return id;
+  }
+}
+
+const test = _test.extend<{ myAPI: MyAPI }>({
+  myAPI: async ({ request }, use) => {
+    await use(new MyAPI(request));
+  }
+});
 
 let companyId;
 let linkedCompanyId;
 
-test.beforeAll(async ({ request }) => {
-  const createNewCompany = async () => {
-    const resp = await request.post('http://localhost:4747', {
-      data: {
-        ts: new Date(),
-      }
-    });
-    const { id } = await resp.json()
-    return id;
-  }
-
-  companyId = await createNewCompany();
-  linkedCompanyId = await createNewCompany();
+test.beforeAll(async ({ myAPI }) => {
+  companyId = await myAPI.newOnboardingCompany();
+  linkedCompanyId = await myAPI.newOnboardingCompany();
 });
 
 test('we have two new ids', () => {
